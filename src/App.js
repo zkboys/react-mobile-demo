e0e7a94b27529bc22eb36f1cd4e7a3f1933ce8b4
zkboys <876213774@qq.com>
2018-10-31 17:12:54 +0800

    props处理

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import UserList from './demo/UserList';
 import UserDetail from './demo/UserDetail';
+import Button from './demo/Button';
 import './App.css';
 
 class App extends Component {
@@ -17,11 +18,21 @@ class App extends Component {
         console.log('App: ', name);
     };
 
+    handleBtnClick = () => {
+        console.log('button clicked!!');
+    };
 
     render() {
         const {job} = this.state;
         return (
             <div className="App">
+                <Button
+                    style={{color: 'green', background: 'yellow'}}
+                    className="test-btn"
+                    onClick={this.handleBtnClick}
+                >我是按钮</Button>
+
+
                 <UserList className="user-list" job={job} getName={this.handleGetName}/>
                 <button onClick={this.handleJobClick}>换工作</button>
                 <UserDetail name="李四" age={23} job="前端"/>
