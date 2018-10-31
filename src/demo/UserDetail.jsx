6d26de343553f948a4917a7b0fcd237e3d82d1d9
zkboys <876213774@qq.com>
2018-10-31 15:10:14 +0800

    init

---
+++ src/demo/UserDetail.jsx
@@ -0,0 +1,39 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+export default class UserDetail extends Component {
+    static defaultProps = {
+        job: 'IT',
+    };
+
+    static propTypes = {
+        name: PropTypes.string.isRequired,
+        age: PropTypes.number.isRequired,
+    };
+
+    state = {};
+
+    componentWillMount() {
+
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        const {
+            name,
+            age,
+            job,
+        } = this.props;
+
+        return (
+            <div>
+                <div>姓名: {name}</div>
+                <div>年龄: {age}</div>
+                <div>工作: {job}</div>
+            </div>
+        );
+    }
+}
