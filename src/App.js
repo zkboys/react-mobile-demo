76ec1e0b24f93b6367a53cbc6b5db7d5501e147d
tobeConfident <838248029@qq.com>
2018-10-31 15:50:47 +0800

    update

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import UserList from './demo/UserList';
+import UserDetail from './demo/UserDetail';
+import './App.css';
+
+class App extends Component {
+    state = {
+        job: 'IT',
+    };
+
+    // 换工作 点击事件
+    handleJobClick = () => {
+        this.setState({job: '前端'});
+    };
+
+
+
+    render() {
+        const {job} = this.state;
+        return (
+            <div className="App">
+                <UserList className="user-list" job={job}/>
+                <button onClick={this.handleJobClick}>换工作</button>
+                <UserDetail name="李四" age={23} job="前端"/>
+            </div>
+        );
+    }
+}
+
+export default App;
