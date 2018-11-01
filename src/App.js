56bd7c2f066d9ff805d00e44b6f16cfa0e773156
tobeConfident <838248029@qq.com>
2018-11-01 17:27:05 +0800

    react静态网页

--- src/App.js
+++ src/App.js
@@ -1,56 +1,14 @@
 import React, {Component} from 'react';
-import UserList from './demo/UserList';
-import UserDetail from './demo/UserDetail';
-<<<<<<< HEAD
-=======
-import Button from './demo/Button';
->>>>>>> master
-import './App.css';
+
+// import './App.css';
+import Index from "./dish/Index.jsx";
 
 class App extends Component {
-    state = {
-        job: 'IT',
-    };
 
-    // 换工作 点击事件
-    handleJobClick = () => {
-        this.setState({job: '前端'});
-    };
-
-<<<<<<< HEAD
-
-=======
-    handleGetName = (name) => {
-        console.log('App: ', name);
-    };
-
-    handleBtnClick = () => {
-        console.log('button clicked!!');
-    };
->>>>>>> master
-
-    render() {
-        const {job} = this.state;
+    render(){
         return (
-            <div className="App">
-<<<<<<< HEAD
-                <UserList className="user-list" job={job}/>
-=======
-                <Button
-                    style={{color: 'green', background: 'yellow'}}
-                    className="test-btn"
-                    onClick={this.handleBtnClick}
-                    title="按钮"
-                >我是按钮</Button>
-
-
-                <UserList className="user-list" job={job} getName={this.handleGetName}/>
->>>>>>> master
-                <button onClick={this.handleJobClick}>换工作</button>
-                <UserDetail name="李四" age={23} job="前端"/>
-            </div>
+            <Index />
         );
     }
 }
-
 export default App;
